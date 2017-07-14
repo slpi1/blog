ed31958baddca634a5a6a0dca988cb9c94e07513
slpi1 <365625906@qq.com>
2017-07-14 16:39:09 +0800

    update password style

--- password/js/password.js
+++ password/js/password.js
@@ -5,22 +5,25 @@ var vm = new Vue({
         userName: '',
         domain: '',
         hash: '',
-        password:''
+        password:'',
+        
+        copy:false
     },
-
+    mounted(){
+        new Clipboard('.btn');
+    },
     methods: {
         getPassword: function () {
             var char = [1,4,7,10,13,16,19,22,25,28,31],
                 g = md5(this.userName + "@" + this.domain + ":" + this.hash),
-                t = ''
-                e = this.$el.password;
+                t = '';
             
             char.forEach(function(i){
                 t += ( i > 15 ) ? g[i].toUpperCase() : g[i];
             })
             
             this.password = t;
-            e.focus();
+            this.copy = true
         }
     }
 })
